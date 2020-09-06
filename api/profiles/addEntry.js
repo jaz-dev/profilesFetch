c14afc4153059d21b7f87efe4bbc6fb1e541d637
Jaswant Kasinedi <jaswantkasinedi@gmail.com>
2020-09-06 02:17:55 -0400

    Update addEntry.js

--- api/profiles/addEntry.js
+++ api/profiles/addEntry.js
@@ -1,7 +1,29 @@
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
 const db = require('../../lib/db')
 const escape = require('sql-template-strings')
 
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 module.exports = async (req, res) => {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  // Rest of the API logic
   await db.query(escape`
 	  INSERT
 	  INTO    compPostInfo
