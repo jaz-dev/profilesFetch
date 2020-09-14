a311819d97b5f2cdb8cb43013f330ef5d06451e8
Jaswant Kasinedi <jaswantkasinedi@gmail.com>
2020-09-14 01:24:37 -0400

    Update newIndex.js

--- api/profiles/newIndex.js
+++ api/profiles/newIndex.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
   const posts = await db.query(escape`
       SELECT *
       FROM compPostInfo
-      ORDER BY date
+      ORDER BY date DESC
     `)
 
   res.status(200).json({ posts})
